2e1ab1632cc578f43b38c2987d7559043c0fa164
GabiDourado <129889799+GabiDourado@users.noreply.github.com>
2023-06-07 15:41:10 -0300

    pagina inicial ebaaaaaaaa

---
+++ assets/js/inicial.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(".per1").on("click", function(){
+        var aberto = document.getElementById("resposta").style.display;
+        if(aberto == 'block')
+            document.getElementById("resposta").style.display = "none";
+        else
+            document.getElementById("resposta").style.display = "block";
+    })
+    $(".per2").on("click", function(){
+        var aberto = document.getElementById("resposta2").style.display;
+        if(aberto == 'block')
+            document.getElementById("resposta2").style.display = "none";
+        else
+            document.getElementById("resposta2").style.display = "block";
+    })
+    $(".per3").on("click", function(){
+        var aberto = document.getElementById("resposta3").style.display;
+        if(aberto == 'block')
+            document.getElementById("resposta3").style.display = "none";
+        else
+            document.getElementById("resposta3").style.display = "block";
+    })
+    $(".per4").on("click", function(){
+        var aberto = document.getElementById("resposta4").style.display;
+        if(aberto == 'block')
+            document.getElementById("resposta4").style.display = "none";
+        else
+            document.getElementById("resposta4").style.display = "block";
+    })
+    $(".per5").on("click", function(){
+        var aberto = document.getElementById("resposta5").style.display;
+        if(aberto == 'block')
+            document.getElementById("resposta5").style.display = "none";
+        else
+            document.getElementById("resposta5").style.display = "block";
+    })
+    $(".per6").on("click", function(){
+        var aberto = document.getElementById("resposta6").style.display;
+        if(aberto == 'block')
+            document.getElementById("resposta6").style.display = "none";
+        else
+            document.getElementById("resposta6").style.display = "block";
+    })
+})
